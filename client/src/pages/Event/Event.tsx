7f5c8f2c9b11b3cde66bfd8486484f6435e08d8b
ValentinKoruev <doopster123@gmail.com>
2025-09-24 22:51:09 +0300

    feat: intial event page

--- client/src/pages/Event/Event.tsx
+++ client/src/pages/Event/Event.tsx
@@ -1,20 +1,30 @@
 import { useParams } from "react-router";
 import styles from "./Event.module.scss";
+import Section from "@components/UI/Section/Section";
+import Button from "@components/UI/Button";
 
 export const EventPage = () => {
   const params = useParams();
   return (
     <div className={styles.Event}>
-      <h1>Event {params.id}</h1>
+      <h1>{params.id}</h1>
       <div className={styles.EventContent}>
-        <section className={styles.Details}>
-          <p>Details about event {params.id}...</p>
-          <div className={styles.Actions}>
-            <button>Edit Event</button>
-            <button>Delete Event</button>
-            <button>Share Event</button>
+        <Section className={styles.Details}>
+          <div className={styles.DetailContent}>
+            <p>Details about event {params.id}...</p>
+            <div className={styles.Actions}>
+              <Button variant="primary" color="primary" border="rounded">
+                Edit Event
+              </Button>
+              <Button variant="primary" color="danger" border="rounded">
+                Delete Event
+              </Button>
+              <Button variant="primary" color="secondary" border="rounded">
+                Share Event
+              </Button>
+            </div>
           </div>
-        </section>
+        </Section>
         <aside className={styles.Participants}>
           <h2>Participants</h2>
           <ul className={styles.ParticipantList}>
