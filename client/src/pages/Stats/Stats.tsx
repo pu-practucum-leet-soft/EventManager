fea59d84a85c0d8aced8a40db072fd2aa53e07bc
ValentinKoruev <doopster123@gmail.com>
2025-09-25 01:04:39 +0300

    feat: stats page with expandable cards

--- client/src/pages/Stats/Stats.tsx
+++ client/src/pages/Stats/Stats.tsx
@@ -1,9 +1,20 @@
+import StatsList from "@components/StatsList";
+import Section from "@components/UI/Section";
+import { events } from "../dummyData";
+
 import styles from "./Stats.module.scss";
 
+// TODO: Maybe add participants list?
 const StatsPage = () => {
   return (
     <div className={styles.Stats}>
       <h1>Statistics</h1>
+      <Section title="Events" className={styles.StatsSection}>
+        <div className={styles.StatsContent}>
+          <p>Number of events: {events.length}</p>
+          <StatsList events={events} />
+        </div>
+      </Section>
     </div>
   );
 };
